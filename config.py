01671eb12c16b28a23bc1f71be75b0a26c14f348
isk <isk@MacBook-Air-3.local>
2025-07-18 19:08:38 +0300

    Load config from environment variables

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ BOT_TOKEN = "8033614301:AAEmSCB0g96RhQh6D_eBEpwZqDtcLsUIvJQ"
 
 # Ваш личный ID в Telegram или ID чата менеджера.
 # Чтобы его узнать, напишите боту @userinfobot, он пришлет ваш ID.
-ADMIN_CHAT_ID = "764788161"
+ADMIN_CHAT_ID = "492652403"
 
 # Строка для подключения к базе данных PostgreSQL, которую мы настроили через Homebrew.
 # Замените `<имя_пользователя>` на ваше имя пользователя в macOS.
 # Чтобы узнать его, введите команду `whoami` в Терминале.
-DATABASE_URL = "postgresql://isk@localhost/tea_shop_db"
+DATABASE_URL = "postgresql://yulbars_db_user:evIVYvgfhj1XknYcJJN0mFCrMolidOP9@dpg-d1t5nb2dbo4c73fvr7og-a/yulbars_db"
 
 # Ссылка, по которой будет доступен ваш Mini App (фронтенд).
 # Когда вы развернете фронтенд, вы замените эту ссылку на настоящую.
